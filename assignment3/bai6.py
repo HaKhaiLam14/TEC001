3c80154de3dbe44cd71b8d2b119463d101a04a60
HaKhaiLam14 <kl18012005@gmail.com>
2026-02-04 21:12:02 +0700

    Update bai6.py

--- assignment3/bai6.py
+++ assignment3/bai6.py
@@ -1,8 +1,10 @@
 s = input()
-s = s.upper()
 
-s2 = s.split()
+def short_cpital(s:str):
+    s = s.upper()
+    s2 = s.split()
+    for i in s2:
+        print(i[0], end='')
 
-for i in s2:
-    print(i[0], end='')
+short_cpital(s)
     
